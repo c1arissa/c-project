2eaf726f8793dde1665868a5e0ceb7308e2d8a75
NR5858 <navinr111@gmail.com>
2018-10-11 14:43:07 +0100

    Moved implementation of quitReceived() from header file to cpp file. Fixed indentation issues.

--- connectionhandler.h
+++ connectionhandler.h
@@ -8,20 +8,26 @@
 
 class ConnectionHandler {
 private:
-	std::deque< std::string > queue_;
-	std::thread thread_;
-	bool quitReceived_;
-	std::mutex mutex_;
-	std::string userIdentifiedAs_;
-private:
-	void processMessages();
+  std::deque< std::string > queue_;
+  std::thread thread_;
+  bool quitReceived_;
+  std::mutex mutex_;
+  std::string userIdentifiedAs_;
+
+        
+  void processMessages();
 public:
-	void start();
-	void queueMessage( const char* buffer, size_t length );
-	bool quitReceived() const {
-		return quitReceived_;
-	}
-	void join();
+  //Starts a new connection by spawning a new thread
+  void start();
+  
+  //Pushes a message onto the end of the queue
+  void queueMessage( const char* buffer, size_t length );
+  
+  //Returns whether or not quit was received
+  bool quitReceived() const;
+  
+  
+  void join();
 };
 
 #endif // ConnectionHandler_h_
